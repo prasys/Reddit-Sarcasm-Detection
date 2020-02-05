783bcd291001d09ce27b456a2bbcef5c0763d6f5
prasys <pradeesh@gmail.com>
2020-02-05 22:05:20 +1300

    added support to read comments from csv to be processed

--- reddit_sarcasm_exec.py
+++ reddit_sarcasm_exec.py
@@ -16,19 +16,13 @@ def comments_to_seq2(c):
 MAX_NB_WORDS = 50000 #2000 unique words?
 MAX_SEQUENCE_LENGTH = 30 #30 word sentences?
 
-key_df =pd.read_csv('data/key.csv', sep='\t')
-train_df = pd.read_csv('data/train-balanced.csv', sep='\t', header=None, names=list(key_df))
-print("Read in CSVs")
-comments = [str(x) for x in list(train_df["comment"])] #add in parent comment later
 tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
-tokenizer.fit_on_texts(comments)
-print("Fitted Tokenizer to text")
-
-input_comments = ["Glad to know I mean so much to you.", "I love fridays"]
+#input_comments = ["yes sir yes", "I  love fridays","Yay that was so much fun when people would go up there with the flag and wait for people to quit so they could get a win."]
+df = pd.read_csv('file_name.csv')
+input_comments = df['Comment'].to_list()
 sequences = tokenizer.texts_to_sequences(input_comments)
 
 data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
 data2 = pad_sequences(comments_to_seq2(input_comments), maxlen=MAX_SEQUENCE_LENGTH)
-
 model = load_model("large_feature_model_2.h5")
 print(model.predict([data, data2]))
